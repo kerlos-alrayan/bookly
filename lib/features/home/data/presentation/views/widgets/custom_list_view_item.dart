70b5deb2ee5235dee16f407bc1f6650d9ddcb3a2
kerlos-alrayan <kerlossheref@gmail.com>
2025-02-24 13:17:37 +0200

    add featured list view to home view

--- lib/features/home/data/presentation/views/widgets/custom_list_view_item.dart
+++ lib/features/home/data/presentation/views/widgets/custom_list_view_item.dart
@@ -7,17 +7,14 @@ class CustomListViewItems extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.23,
-      child: AspectRatio(
-        aspectRatio: 2.5 / 4,
-        child: Container(
-          decoration:BoxDecoration(
-            borderRadius: BorderRadius.circular(12),
-            image: const DecorationImage(
-                image: AssetImage(AssetsData.testImage),
-                fit: BoxFit.cover
-            ),
+    return AspectRatio(
+      aspectRatio: 2.5 / 4,
+      child: Container(
+        decoration:BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          image: const DecorationImage(
+              image: AssetImage(AssetsData.testImage),
+              fit: BoxFit.cover
           ),
         ),
       ),
--- lib/features/home/data/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/data/presentation/views/widgets/home_view_body.dart
@@ -10,8 +10,26 @@ class HomeViewBody extends StatelessWidget {
     return Column(
       children: [
         CustomAppBar(),
-        CustomListViewItems(),
+        FeaturedBooksListView(),
       ],
+    );
+  }
+}
+
+class FeaturedBooksListView extends StatelessWidget {
+  const FeaturedBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.23,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) => Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          child: CustomListViewItems(),
+        ),
+      ),
     );
   }
 }
