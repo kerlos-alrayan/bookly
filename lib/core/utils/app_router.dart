6c5d21a384ec26e576c485a83162488f5edb7275
kerlos-alrayan <kerlossheref@gmail.com>
2025-03-06 01:56:58 +0200

    book details view done😉

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -3,7 +3,6 @@ import 'package:bookly/features/home/data/models/book_model.dart';
 import 'package:bookly/features/home/data/repos/home_repo_impl.dart';
 import 'package:bookly/features/home/presentation/manger/similar_books_cubit/similar_books_cubit.dart';
 import 'package:bookly/features/home/presentation/views/home_view.dart';
-import 'package:bookly/test_image/ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
